985d182ddafd0d6e8800abeaf7f5822d424addcd
Valeria Rubio Herrera <valeriarubioh@gmail.com>
2023-12-28 18:20:16 -0500

    Is subsequence

---
+++ IsSubsequence/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        String s = "";
+        String t = "ahbgdc";
+        Solution sol = new Solution();
+        System.out.println(sol.isSubsequence(s,t));
+    }
+}
